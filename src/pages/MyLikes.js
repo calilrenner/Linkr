bc2985d09193ffe0050af7d4a1141523cee72e00
thipereira02 <thiago.spereira02@gmail.com>
2021-09-22 18:06:39 -0300

    Fixing bugs (adding text when doens't have posts or likes and loading while the request hasn't come back yet

--- src/pages/MyLikes.js
+++ src/pages/MyLikes.js
@@ -1,18 +1,22 @@
 import { useEffect, useState, useContext } from "react";
+import styled from "styled-components";
 import { getMyLikes } from "../service/api.service";
 import UserContext from "../contexts/UserContext";
 import Post from "../components/Post";
 import Trending from "../components/Trending";
 import Header from "../components/Header";
-import { Main, Title } from "./mainStyles";
+import { Loader, Main, Title } from "./mainStyles";
 
 export default function MyLikes() {
   const { userData } = useContext(UserContext);
   const [likedPosts, setLikedPosts] = useState({});
+  const [load, setLoad] = useState(false);
 
   useEffect(() => {
-    getMyLikes({ token: userData.token }).then((r) =>
+    getMyLikes({ token: userData.token }).then((r) => {
       setLikedPosts(r.data.posts)
+      setLoad(true);
+    }
     );
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
   return (
@@ -20,13 +24,27 @@ export default function MyLikes() {
       <Header />
       <Main>
         <Title>my likes</Title>
-        {likedPosts.length > 0 ? (
-          likedPosts.map((post, index) => <Post key={index} {...post} />)
-        ) : (
-          <Title>Você ainda não curtiu nada :(</Title>
-        )}
+        {load ?
+          (likedPosts.length === 0 ?
+            <Title>
+              Você ainda não curtiu nada ☹️
+            </Title>
+            :
+            likedPosts.map((post, index) => (
+              <Post key={index} {...post} />
+            ))
+          )
+          :
+          <Container>
+            <Loader />
+          </Container>
+        }
       </Main>
       <Trending />
     </>
   );
 }
+
+const Container = styled.div`
+  margin: 0 150px;
+`;
--- src/pages/MyPosts.js
+++ src/pages/MyPosts.js
@@ -1,21 +1,26 @@
 import { useContext, useEffect, useState } from "react";
+import styled from "styled-components";
 import UserContext from "../contexts/UserContext";
 import { getUserPosts } from "../service/api.service";
 import Header from "../components/Header";
 import Trending from "../components/Trending";
 import Post from "../components/Post";
-import { Main, Title } from "./mainStyles";
+import { Main, Title, Loader } from "./mainStyles";
 
 export default function MyPosts() {
   const { userData, onChangePost } = useContext(UserContext);
   const [userPosts, setUserPosts] = useState([]);
+  const [load, setLoad] = useState(false);
 
   useEffect(() => {
     const id = userData.user.id;
     const token = { token: userData.token };
     const req = getUserPosts(id, token);
 
-    req.then((res) => setUserPosts(res.data.posts));
+    req.then((res) => {
+      setUserPosts(res.data.posts)
+      setLoad(true)
+    });
   }, [onChangePost]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
@@ -23,11 +28,27 @@ export default function MyPosts() {
       <div>
         <Header />
         <Title>my posts</Title>
-        {userPosts.map((u, i) => (
-          <Post key={i} {...u} />
-        ))}
+        {load ? 
+          (userPosts.length === 0 ?
+            <Title>
+              Você ainda não curtiu nada ☹️
+            </Title>
+            :
+            userPosts.map((u, i) => (
+              <Post key={i} {...u} />
+            ))
+          )
+          :
+          <Container>
+            <Loader />
+          </Container>
+        }
       </div>
       <Trending />
     </Main>
   );
 }
+
+const Container = styled.div`
+  margin: 0 150px;
+`;
