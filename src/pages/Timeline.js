283f2a7e900d787b538921d508041a778704aee4
Calil-Silva <ka.renner@hotmail.com>
2021-09-25 22:54:12 -0300

    error

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -5,7 +5,6 @@ import { getFollowsPosts, getFollows } from "../service/api.service";
 import Header from "../components/Header";
 import Trending from "../components/Trending";
 import CreateNewPost from "../components/CreateNewPost";
-import axios from "axios";
 import styled from "styled-components";
 import { colors } from "../globalStyles";
 import SearchUser from "../components/SearchUser";
@@ -18,92 +17,6 @@ import {
 } from "./mainStyles";
 
 export default function Timeline() {
-  const [posts, setPosts] = useState("");
-  const [errPosts, SetErrPosts] = useState("");
-  const { userData, onChangePost, setOnChangePost } = useContext(UserContext);
-  const [postsIds, setPostsIds] = useState([]);
-  const [trasnfer, setTrasnfer] = useState(false)
-  let higher = Number.POSITIVE_INFINITY;
-  let lower = Number.NEGATIVE_INFINITY;
-  const [lastPostId, setLastPostId] = useState(0);
-  const [firstPostId, setFirstPostId] = useState(0);
-
-  const [loading, setLoading] = useState(true);
-  const [hasMore, setHasMore] = useState(false);
-  const observer = useRef();
-  function setConfig() {
-    return { headers: { Authorization: `Bearer ${userData.token}` } };
-  }
-
-  const lastPostRef = useCallback(node => {
-    if(observer.current) observer.current.disconnect();
-      observer.current = new IntersectionObserver(entries => {
-        if(entries[0].isIntersecting) {
-
-          axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v3/linkr/following/posts', setConfig())
-            .then(res => console.log(res.data))
-          console.log(lastPostId, firstPostId)
-    
-        }
-      })
-      if(node) observer.current.observe(node)
-  }, [loading, hasMore]);
-
-  function postRepost(post) {
-    if(post.repostId) {
-      return post.repostId;
-    } else {
-      return post.id;
-    }
-  }
-
-  useEffect(() => {
-    timelinePosts();
-  }, [onChangePost]);
-
-  function timelinePosts() {
-    getPosts(userData.token)
-      .then((res) => {
-        setPosts(res.data.posts);
-        setTrasnfer(!trasnfer);
-      })
-      .catch((err) =>
-        SetErrPosts(
-          "Houve uma falha ao obter os posts, por favor atualize a página"
-        )
-      );
-  }
-
-  useEffect(() => {
-    if(posts.length > 0) {
-        setPostsIds(posts.map(post => postRepost(post)));
-    }
-  }, [posts, trasnfer])
-
-  useEffect(() => {
-    if(postsIds.length !== 0) {
-      postsIds.forEach(id => {
-        if(id < higher) {
-          higher = id;
-          setFirstPostId(higher);
-        }
-      })
-    };
-    if(postsIds.length !== 0) {
-      postsIds.forEach(id => {
-        if(id > lower) {
-          lower = id;
-          setLastPostId(lower);
-        }
-      })
-    }
-  }, [postsIds, posts])
-
-  console.log(lastPostId, firstPostId)
-
-  function loadPosts() {
-    if (errPosts !== "") {
-      return <ErrorMsg>{errPosts}</ErrorMsg>;
   const { userData, onChangePost, setOnChangePost } = useContext(UserContext);
   const [followedPosts, setFollowedPosts] = useState("");
   const [followedUsers, setFollowedUsers] = useState([]);
